459ae6b86fb25f47ef472daaf754c8b9bb014436
Omar Illescas Escobedo <115673454+Illescas14@users.noreply.github.com>
2025-09-11 19:55:57 -0600

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,10 @@
 // ----------- Contact form -----------
 document.getElementById("contact-form")?.addEventListener("submit", (e) => {
   e.preventDefault();
-  alert("Gracias por contactarnos. Te responderemos pronto por WhatsApp o correo.");
+  alert("Gracias por contactarnos. Te responderemos pronto.");
   e.target.reset();
 });
 
 // ----------- Year auto update -----------
 document.getElementById("year").textContent = new Date().getFullYear();
+
